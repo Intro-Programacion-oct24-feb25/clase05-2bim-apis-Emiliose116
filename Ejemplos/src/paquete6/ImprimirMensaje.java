62e969063e2c64d4e2b85a89410c926467e6685d
unknown <ecueva611@gmail.com>
2025-01-16 11:38:11 -0500

    Thnkll

---
+++ Ejemplos/src/paquete6/ImprimirMensaje.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete6;
+
+/**
+ *
+ * @author utpl
+ */
+public class ImprimirMensaje {
+    public static void imprimirMensaje(int a, int b, int suma, int multiplicacion){
+    System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La multiplicacion de %d * %d es igual a: %d\n",
+                a,b,
+                suma,
+                a, b,
+                multiplicacion);
+}
+}
